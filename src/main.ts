0441e82af66c3a42bb946e5c7f301050373ef866
VT <vt@Mac-mini.local>
2021-11-22 15:15:58 +0800

    在全局注册请求错误的过滤器

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from "./core/filter/http-exception.filter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // 注册全局错误的过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix('api')
   await app.listen(3306);
 }
